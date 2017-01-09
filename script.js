97b898fca42f4eccdc57481863041f0cadea7f2f
Lia Petronio <lia.marie.petronio@gmail.com>
2017-01-09 16:00:07 -0500

    k

---
+++ script.js
@@ -0,0 +1,30 @@
+ $( window ).load(function() {
+     $(".page").hide(); 
+     $("#schedule-pg").show();
+   });
+
+
+$(document).ready(function(){
+    $(".nbtn").on('click', function(){
+      $(".nbtn").removeClass("active")
+})});
+
+
+$(document).ready(function(){
+	$("#schedule-btn").on('click', function(){
+    	$(".page").hide(); 
+    	$("#schedule-pg").show();
+})});
+
+
+$(document).ready(function(){
+	$("#syllabus-btn").on('click', function(){
+    	$(".page").hide(); 
+    	$("#syllabus-pg").show();
+})});
+
+$(document).ready(function(){
+	$("#assignments-btn").on('click', function(){
+    	$(".page").hide(); 
+    	$("#assignments-pg").show();
+})});
